d13ba1ef4fd694aca73312005602110166b2600e
Elman Huseynov <thehuseyn0w@gmail.com>
2024-06-11 00:33:36 +0200

    Add context menu

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, Tray, ipcMain } = require('electron');
 const path = require('path');
 require('electron-reload')(__dirname, {
   electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
 });
 
 let mainWindow;
+let tray;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -21,8 +22,43 @@ function createWindow() {
   mainWindow.setBackgroundColor('#FFFFFF');
 }
 
+function createTray() {
+  tray = new Tray(path.join(__dirname, 'icon.png'));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Vocabularify',
+      enabled: true
+    },
+    {
+      label: 'Mode',
+      submenu: [
+        {
+          label: 'Light Mode',
+          type: 'radio',
+          checked: true,
+          click: () => {
+            mainWindow.webContents.send('set-mode', 'light');
+          }
+        },
+        {
+          label: 'Dark Mode',
+          type: 'radio',
+          click: () => {
+            mainWindow.webContents.send('set-mode', 'dark');
+          }
+        }
+      ]
+    }
+  ]);
+
+  tray.setToolTip('Vocabularify');
+  tray.setTitle('Vocabularify');
+  tray.setContextMenu(contextMenu);
+}
+
 app.whenReady().then(() => {
   createWindow();
+  createTray();
 
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -33,8 +69,6 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
 });
 
-// Expose a method to resize the window from the renderer process
-const { ipcMain } = require('electron');
 ipcMain.on('resize-window', (event, width, height) => {
   if (mainWindow) {
     mainWindow.setSize(width, height);
